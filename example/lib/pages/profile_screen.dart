b5987299b5026b2e9059eaddd86ad58e7097cbb8
Nguyen Tan <tan.nguyen@widosoft.com>
2024-06-29 14:49:14 +0700

    fix: add scroll to profile

--- example/lib/pages/profile_screen.dart
+++ example/lib/pages/profile_screen.dart
@@ -70,138 +70,140 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: SafeArea(
         top: false,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-                width: MediaQuery.of(context).size.width,
-                height: 215,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/background_profile.png'),
-                    fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 215,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/background_profile.png'),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'My Profile',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'My Profile',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 34,
+                                  backgroundImage:  _data?['avatar'] != null
+                                      ? NetworkImage(_data?['avatar'] as String)
+                                      : null,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${_data?['fullname'] ?? ''}',
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 16),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text('${_data?['address'] ?? ''}',
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                        )),
+                                  ],
+                                )
+                              ],
+                            ),
+                            InkWell(
+                              onTap: () => Navigator.pushNamed(context, EditProfile.route),
+                              child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10.0, horizontal: 20),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.15),
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: const Center(
+                                      child: Text(
+                                    'Edit Profile',
+                                    style: TextStyle(color: Colors.white),
+                                  ))),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
+              ListView(
+                padding: const EdgeInsets.all(25),
+                shrinkWrap: true,
+                children: section
+                    .map((item) {
+                      final section   = item['sections']! as List;
+                      return Padding(
+                      padding: const EdgeInsets.only(bottom: 25.0),
+                      child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              CircleAvatar(
-                                radius: 34,
-                                backgroundImage:  _data?['avatar'] != null
-                                    ? NetworkImage(_data?['avatar'] as String)
-                                    : null,
+                              Text(
+                                '${item['title']}',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
                               ),
                               const SizedBox(
-                                width: 10,
+                                height: 15,
                               ),
                               Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${_data?['fullname'] ?? ''}',
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 16),
+                                children: section.map((e) =>  Padding(
+                                  padding: const EdgeInsets.only(bottom: 10.0),
+                                  child: Container(
+                                    padding: const EdgeInsets.all(12),
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 1, color: const Color(0xFFF3F3F3)),
+                                        borderRadius: BorderRadius.circular(20)),
+                                    child: Row(
+                                      children: [
+                                        Image.asset(e['icon'] as String),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text('${e['title']}')
+                                      ],
+                                    ),
                                   ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text('${_data?['address'] ?? ''}',
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                      )),
-                                ],
+                                )).toList(),
                               )
                             ],
                           ),
-                          InkWell(
-                            onTap: () => Navigator.pushNamed(context, EditProfile.route),
-                            child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 10.0, horizontal: 20),
-                                decoration: BoxDecoration(
-                                    color: Colors.white.withOpacity(0.15),
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: const Center(
-                                    child: Text(
-                                  'Edit Profile',
-                                  style: TextStyle(color: Colors.white),
-                                ))),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )),
-            ListView(
-              padding: const EdgeInsets.all(25),
-              shrinkWrap: true,
-              children: section
-                  .map((item) {
-                    final section   = item['sections']! as List;
-                    return Padding(
-                    padding: const EdgeInsets.only(bottom: 25.0),
-                    child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${item['title']}',
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Column(
-                              children: section.map((e) =>  Padding(
-                                padding: const EdgeInsets.only(bottom: 10.0),
-                                child: Container(
-                                  padding: const EdgeInsets.all(12),
-                                  decoration: BoxDecoration(
-                                      border: Border.all(
-                                          width: 1, color: const Color(0xFFF3F3F3)),
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Row(
-                                    children: [
-                                      Image.asset(e['icon'] as String),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      Text('${e['title']}')
-                                    ],
-                                  ),
-                                ),
-                              )).toList(),
-                            )
-                          ],
-                        ),
-                  );
-                  })
-                  .toList(),
-            )
-          ],
+                    );
+                    })
+                    .toList(),
+              )
+            ],
+          ),
         ),
       ),
     );
